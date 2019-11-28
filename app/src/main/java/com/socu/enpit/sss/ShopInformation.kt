061c72f10e42e6264634c0006bcbca0320fe2ff2
enPiT-P13 <f217040@ed.socu.ac.jp>
2019-11-28 09:48:33 +0900

    //Commit by Fujimoto

---
+++ app/src/main/java/com/socu/enpit/sss/ShopInformation.kt
@@ -0,0 +1,12 @@
+package com.socu.enpit.sss
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShopInformation : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_information)
+    }
+}
