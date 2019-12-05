4cb63ca40b7030398acfb5fb59e898203518511b
enPiT-P12 <f217029@ed.socu.ac.jp>
2019-12-05 11:02:54 +0900

    RequestActivityでCloudDataManagerを使用するようにした

--- app/src/main/java/com/socu/enpit/sss/RequestActivity.kt
+++ app/src/main/java/com/socu/enpit/sss/RequestActivity.kt
@@ -1,23 +1,32 @@
 package com.socu.enpit.sss
 
 import android.os.Bundle
-import android.text.TextUtils
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import java.util.ArrayList
-
-import com.nifcloud.mbaas.core.NCMB
-import com.nifcloud.mbaas.core.NCMBException
-import com.nifcloud.mbaas.core.NCMBObject
 import kotlinx.android.synthetic.main.activity_request.*
+import kotlinx.android.synthetic.main.activity_shop_information.*
 
 
 class RequestActivity : AppCompatActivity() {
 
+    private val adapter = RequestAdapter(ArrayList(), this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // このActivityで表示させるレイアウトをactivity_request.xmlに設定する。
+        setContentView(R.layout.activity_request)
 
+        // レイアウトとRequestAdapterを連携させる
+        requestRecyclerView.layoutManager = LinearLayoutManager(this)
+        requestRecyclerView.adapter = adapter
+
+        // activity_request.xmlの中のrequestRecyclerViewに
+        // CloudDataManagerから受け取った文字列を表示する。
+        val requestDataList = CloudDataManager.getRequestDataList()
+        for (request in requestDataList) adapter.addItem(request)
+
+        // 戻るボタンを押したときの処理
+        back_button.setOnClickListener{ finish() }
     }
 }
 
