6942508a6c67c788e6eb2faf671b6cbfa5da44ba
enPiT-P12 <f217029@ed.socu.ac.jp>
2019-12-05 10:00:41 +0900

    RequestActivity追加

--- app/src/main/java/com/socu/enpit/sss/RequestActivity.kt
+++ app/src/main/java/com/socu/enpit/sss/RequestActivity.kt
@@ -1,12 +1,23 @@
 package com.socu.enpit.sss
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import java.util.ArrayList
+
+import com.nifcloud.mbaas.core.NCMB
+import com.nifcloud.mbaas.core.NCMBException
+import com.nifcloud.mbaas.core.NCMBObject
+import kotlinx.android.synthetic.main.activity_request.*
+
 
 class RequestActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_request)
+        setContentView(R.layout.activity_main)
+
     }
 }
+
